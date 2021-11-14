9f0236e3fdc8347d70704d487b9c0a885d18cb88
Jai Prasad <jaiprasad266@gmail.com>
2021-11-14 19:28:18 +0530

    bmicalculator update

--- javascript/bmicalculator.js
+++ javascript/bmicalculator.js
@@ -1,7 +1,16 @@
-function bmiCalculator(weight,height) {
+function bmiCalculator(weight, height) {
+    var bmi = weight / (height * height);
 
-    return weight/(height*height);
+    var interpretation = "";
+
+    if (bmi < 18.5) {
+        interpretation = "Your BMI is " + Math.round(bmi) + ", so you are underweight.";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        interpretation = "Your BMI is " + Math.round(bmi) + ", so you have a normal weight.";
+    } else {
+        interpretation = "Your BMI is " + Math.round(bmi) + ", so you are over weight.";
+    }
+    return interpretation;
 }
 
-var bmi = bmiCalculator(65,1.8);
-console.log("Your BMI is " + Math.round(bmi));
+console.log(bmiCalculator(100,2));
