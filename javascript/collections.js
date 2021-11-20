77199590f346df3559db7e6eaa64e6a2a07ddc38
Jai Prasad <jaiprasad266@gmail.com>
2021-11-20 13:17:25 +0530

    collections array

---
+++ javascript/collections.js
@@ -0,0 +1,9 @@
+var guestList = ["Tony Stark","Peter Parker","Captain America","Thor","Hawkeye","BlackWidow"];
+
+var guestName = "Peter Parker";
+
+if(guestList.includes(guestName)){
+    console.log("Welcome to the party " + guestName);
+}else{
+    console.log("Sorry");
+}
